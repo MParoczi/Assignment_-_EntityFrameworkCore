3f8eb23ae8d553b54769fa9366fba0dbb2920759
Márk Paróczi <mark.paroczi@gmail.com>
2020-03-10 20:21:18 +0100

    Reformat Startup.cs

--- Blog/Startup.cs
+++ Blog/Startup.cs
@@ -63,8 +63,8 @@ namespace Blog
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    "default",
+                    "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
