1ea163fcbeb21b5480dfa2d73ebb7332c509fd0f
Márk Paróczi <mark.paroczi@gmail.com>
2020-03-10 15:13:35 +0100

    Implement class "UserEntityTypeConfiguration"

--- Blog/Data/Contexts/Configurations/UserEntityTypeConfiguration.cs
+++ Blog/Data/Contexts/Configurations/UserEntityTypeConfiguration.cs
@@ -1,7 +1,26 @@
-﻿namespace Blog.Data.Contexts.Configurations
+﻿using System.Collections.Generic;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Newtonsoft.Json;
+
+namespace Blog.Data.Contexts.Configurations
 {
-    public class UserEntityTypeConfiguration
+    public class UserEntityTypeConfiguration : IEntityTypeConfiguration<User>
     {
-        
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("user");
+            builder.HasKey(prop => prop.Id);
+            builder.Property(prop => prop.Id).HasColumnName("id").ValueGeneratedOnAdd();
+            builder.Property(prop => prop.FirstName).HasColumnName("first_name").HasMaxLength(50).IsRequired();
+            builder.Property(prop => prop.LastName).HasColumnName("last_name").HasMaxLength(20).IsRequired();
+            builder.Property(prop => prop.Posts).HasColumnName("posts").HasConversion(
+                posts =>
+                    JsonConvert.SerializeObject(posts,
+                        new JsonSerializerSettings {NullValueHandling = NullValueHandling.Ignore}),
+                posts => JsonConvert.DeserializeObject<List<Post>>(posts,
+                        new JsonSerializerSettings {NullValueHandling = NullValueHandling.Ignore}));
+        }
     }
 }
