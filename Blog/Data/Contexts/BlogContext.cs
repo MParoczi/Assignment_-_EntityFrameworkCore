3d1e0f9e3b536202bf82adec28ada323e8e72484
Márk Paróczi <mark.paroczi@gmail.com>
2020-03-10 14:54:24 +0100

    Implement class "BlogContext"

--- Blog/Data/Contexts/BlogContext.cs
+++ Blog/Data/Contexts/BlogContext.cs
@@ -1,7 +1,22 @@
-﻿namespace Blog.Data.Contexts
+﻿using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data.Contexts
 {
-    public class BlogContext
+    public class BlogContext : DbContext
     {
-        
+        public BlogContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new PostEntityTypeConfiguration());
+        }
     }
 }
